442ffed0804059ef7c09d3465237ce57eff71b51
cplkimth <cplkimth@hotmail.com>
2018-08-08 11:29:24 +0900

    '

--- main.cpp
+++ main.cpp
@@ -6,59 +6,43 @@ using namespace std;
 #define TRUE 1 // 전처리기 지시어
 #define MAX_VALUE 10
 #define STRIKE_TO_FINISH 3
-
-/*
-casing
-
-Pascal :ThisIsName
-Camel :	thisIsName
-Upper : THIS_IS_NAME
-*/
-
+#define DIGIT 3
+// debug
 int main()
 {
 	// 1. 0~9 사이의 중복되지 않는 세 개의 난수를 생성한다. (정답)
-	int answer0;
-	int answer1;
-	int answer2;
-
+	int answers[DIGIT];
+	
 	while (TRUE)
 	{
-		answer0 = rand() % MAX_VALUE;
-		answer1 = rand() % MAX_VALUE;
-		answer2 = rand() % MAX_VALUE;
+		for (int i = 0; i < DIGIT; i++)
+			answers[i] = rand() % MAX_VALUE;
 
-		if (answer0 != answer1 && answer1 != answer2 && answer2 != answer0)
+		if (answers[0] != answers[1] && answers[1] != answers[2] && answers[2] != answers[0])
 			break;
 	}
 
 	cout << "[정답] ";
-	cout << answer0 << ' ';
-	cout << answer1 << ' ';
-	cout << answer2 << ' ';
+	for (int i = 0; i < DIGIT; i++)
+		cout << answers[i] << ' ';
 	cout << endl;
 
 
 	while (TRUE)
 	{
 		// 2. 사용자가 세 개의 숫자를 입력한다. (추측)
-		int guess0;
-		int guess1;
-		int guess2;
-
-		cin >> guess0;
-		getwchar(); // 엔터를 지움
-		cin >> guess1;
-		getwchar(); // 엔터를 지움
-		cin >> guess2;
-		getwchar(); // 엔터를 지움
+		int guesses[DIGIT];
+		
+		for (int i = 0; i < DIGIT; i++)
+		{
+			cin >> guesses[i];
+			getwchar(); // 엔터를 지움
+		}
 
 		cout << "[추측] ";
-		cout << guess0 << ' ';
-		cout << guess1 << ' ';
-		cout << guess2 << ' ';
+		for (int i = 0; i < DIGIT; i++)
+			cout << guesses[i] << ' ';
 		cout << endl;
-
 
 
 		// 3. 결과를 계산한다.
@@ -66,27 +50,19 @@ int main()
 		int ball = 0;
 		int out = 0;
 
-		if (answer0 == guess0)
-			strike++;
-		else if (answer0 == guess1 || answer0 == guess2)
-			ball++;
-		else
-			out++;
+		for (int i = 0; i < DIGIT; i++)
+		{
+			int j = (i + 1) % 3;
+			int k = (i + 2) % 3;
 
-		if (answer1 == guess1)
-			strike++;
-		else if (answer1 == guess2 || answer1 == guess0)
-			ball++;
-		else
-			out++;
-
-		if (answer2 == guess2)
-			strike++;
-		else if (answer2 == guess0 || answer2 == guess1)
-			ball++;
-		else
-			out++;
-
+			if (answers[i] == guesses[i])
+				strike++;
+			else if (answers[i] == guesses[j] || answers[i] == guesses[k])
+				ball++;
+			else
+				out++;
+		}
+		
 
 		// 4. 결과를 출력한다.
 		cout << "[S]" << strike << " [B]" << ball << " [O]" << out << endl;
