20e04ca27f4f6d5f3de94912d0bb48254d0571ac
cplkimth <cplkimth@hotmail.com>
2018-08-09 15:40:52 +0900

    .

--- main.cpp
+++ main.cpp
@@ -4,17 +4,13 @@
 
 using namespace std;
 
-#define TRUE 1 // 전처리기 지시어
+#pragma region 상수
+#define TRUE 1
+#define FALSE 0
 #define MAX_VALUE 10
 #define STRIKE_TO_FINISH 3
-#define DIGIT 3
-
-//typedef struct 
-//{
-//	int Strike;
-//	int Ball;
-//	int Out;
-//}Result;
+#define DIGIT 3  
+#pragma endregion
 
 void PrintNumbers(const char* prefix, int* numbers)
 {
@@ -24,11 +20,8 @@ void PrintNumbers(const char* prefix, int* numbers)
 	cout << endl;
 }
 
-int main()
+void GenerateAnswers(int* answers)
 {
-	// 1. 0~9 사이의 중복되지 않는 세 개의 난수를 생성한다. (정답)
-	int answers[DIGIT];
-
 	while (TRUE)
 	{
 		for (int i = 0; i < DIGIT; i++)
@@ -39,49 +32,90 @@ int main()
 	}
 
 	PrintNumbers("[정답] ", answers);
+}
+
+void InputGuesses(int* guesses)
+{
+	for (int i = 0; i < DIGIT; i++)
+	{
+		cin >> guesses[i];
+		getwchar(); // 엔터를 지움
+	}
+
+	PrintNumbers("[추측] ", guesses);
+}
+
+Result CalculateResult(int* answers, int* guesses)
+{
+	Result result;
+	result.strike = 0;
+	result.ball = 0;
+	result.out = 0;
+
+	for (int i = 0; i < DIGIT; i++)
+	{
+		int j = (i + 1) % 3;
+		int k = (i + 2) % 3;
+
+		if (answers[i] == guesses[i])
+			result.strike++;
+		else if (answers[i] == guesses[j] || answers[i] == guesses[k])
+			result.ball++;
+		else
+			result.out++;
+	}
+
+	result;
+}
+
+void PrintResult(Result result)
+{
+	cout << "[S]" << result.strike << " [B]" << result.ball << " [O]" << result.out << endl;
+}
+
+int IsThreeStrike(Result result)
+{
+	if (result.strike == STRIKE_TO_FINISH)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+int MoreThan4Points(Result result)
+{
+	int point = result.strike * 2 + result.ball;
+
+	return point >= 4 ? TRUE : FALSE;
+}
+
+int main()
+{
+	// 1. 0~9 사이의 중복되지 않는 세 개의 난수를 생성한다. (정답)
+	int answers[DIGIT];
+	GenerateAnswers(answers);
 
 
 	while (TRUE)
 	{
 		// 2. 사용자가 세 개의 숫자를 입력한다. (추측)
 		int guesses[DIGIT];
-
-		for (int i = 0; i < DIGIT; i++)
-		{
-			cin >> guesses[i];
-			getwchar(); // 엔터를 지움
-		}
-
-		PrintNumbers("[추측] ", guesses);
+		InputGuesses(guesses);
 
 
 		// 3. 결과를 계산한다.
-		Result result;
-		result.strike = 0;
-		result.ball = 0;
-		result.out = 0;
-
-
-		for (int i = 0; i < DIGIT; i++)
-		{
-			int j = (i + 1) % 3;
-			int k = (i + 2) % 3;
-
-			if (answers[i] == guesses[i])
-				result.strike++;
-			else if (answers[i] == guesses[j] || answers[i] == guesses[k])
-				result.ball++;
-			else
-				result.out++;
-		}
+		Result result = CalculateResult(answers, guesses);
 
 
 		// 4. 결과를 출력한다.
-		cout << "[S]" << result.strike << " [B]" << result.ball << " [O]" << result.out << endl;
+		PrintResult(result);
 
 
 		// 5. 3스트라이커가 아니면 2번으로 돌아간다.
-		if (result.strike == STRIKE_TO_FINISH)
+		int isValidReult;
+		isValidReult = IsThreeStrike(result);
+		//isValidReult = MoreThan4Points(result);
+
+		if (isValidReult)
 			break;
 	}
 
